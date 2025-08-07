a61694edb22f630f14e1e568539b77b051e37375
kirill9617 <kirill9617@yandex.ru>
2025-08-07 11:28:01 +0200

    Update otp_binary_sensor.h

--- components/otp_sensor/otp_binary_sensor.h
+++ components/otp_sensor/otp_binary_sensor.h
@@ -21,6 +21,7 @@ class TOTPBinarySensor : public Component, public binary_sensor::BinarySensor {
   void set_time_step(int time_step) { this->time_step_ = time_step; }
   void set_digits(int digits) { this->digits_ = digits; }
 
+  std::string get_postfix();
   std::string get_otp();
   std::string get_otp(uint64_t step);
   // template<typename T> void set_upper_threshold(T upper_threshold) { this->upper_threshold_ = upper_threshold; }
@@ -32,6 +33,7 @@ class TOTPBinarySensor : public Component, public binary_sensor::BinarySensor {
   int interval_;
   int time_step_;
   int digits_;
+  std::string postfix; 
   // TemplatableValue<float> upper_threshold_{};
   // TemplatableValue<float> lower_threshold_{};
 };
